50c40cf7ff8f989c62fb8f3943c0964233f1729b
Eric Moore <ericmoore0709@gmail.com>
2024-03-26 13:36:44 -0700

    Fixed form filling bug

--- app.py
+++ app.py
@@ -4,18 +4,15 @@ from stories import Story, story, StoryList, storyList
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def form():
+@app.route('/<id>', methods=['GET', 'POST'])
+def displayFillForm(id: str):
+
     if request.method == 'POST':
         answers = request.form
+        story = storyList.find(id)
         result = story.generate(answers)
-        return render_template('result.html', result=result, title="Result")
-    else:
-        return render_template('form.html', story=story, title="Form")
+        return render_template('result.html', story=story, result=result, title="Result")
 
-
-@app.route('/<id>')
-def displayFillForm(id: str):
     story = storyList.find(id)
     return render_template('form.html', story=story, title="Form")
 
@@ -29,6 +26,6 @@ def new():
         return render_template('new.html', story=Story(""), title="New")
 
 
-@app.route('/stories')
+@app.route('/')
 def displayStories():
     return render_template('stories.html', stories=storyList, title="All Stories")
